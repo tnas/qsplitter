309df7eea0edcb437c674b5b37fd32118c8c5a3d
Thiago Nascimento <nascimenthiago@gmail.com>
2024-09-17 15:36:31 -0300

    First commit

---
+++ src/main/java/com/dzone/tnas/qsplitter/App.java
@@ -0,0 +1,10 @@
+package com.dzone.tnas.qsplitter;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
